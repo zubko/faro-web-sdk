f8d6b55509d9eb339234b9f5e0472b81fc7b2a63
marco.schaefer <marco.schaefer@grafana.com>
2023-03-13 16:37:45 +0100

    wip

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 import { getEnvConfig, getPublicEnvConfig } from './src/common';
 
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react()],
+    plugins: [react(), visualizer()],
     server: {
       watch: {
         awaitWriteFinish: true,
