3a68f59687d04657210eb43dc9a86ee93a0d9fd4
marco.schaefer <marco.schaefer@grafana.com>
2023-03-15 14:31:44 +0100

    chore: add bundle visualizer

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig, loadEnv } from 'vite';
 
 import { getEnvConfig, getPublicEnvConfig } from './src/common';
 
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react()],
+    plugins: [react(), visualizer()],
     server: {
       watch: {
         awaitWriteFinish: true,
