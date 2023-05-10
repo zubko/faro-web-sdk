77e984584d10b5646aad811204a84c19332fd03f
Jack Baldry <jack.baldry@grafana.com>
2023-05-10 15:56:15 +0100

    Add local development environment
    Run `make docs` from the `docs` directory.
    The default build requires a local checkout of the website repository.
    
    To avoid this, run `make DOCS PROJECTS=grafana-cloud/frontend-observability/faro-web-sdk`.
    To make the for yourself, permanently, add `PROJECTS := grafana-cloud/frontend-observability/faro-web-sdk` to `variables.mk.local`.
    
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

---
+++ docs/variables.mk
@@ -0,0 +1,4 @@
+# List of projects to provide to the make-docs script.
+PROJECTS = grafana-cloud/frontend-observability/faro-web-sdk website
+# Mount virtual directories before building the website.
+export WEBSITE_MOUNTS=true
